16e12c25fc8f19b0ba79e1dabab21d42229024d3
Jeriel <jeriel.verissimo@protonmail.com>
2020-05-01 09:02:43 -0300

    Extract window initialization

--- src/main.rs
+++ src/main.rs
@@ -4,40 +4,59 @@ use std::time::Duration;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
+use sdl2::render;
+use sdl2::video;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let sdl_context = sdl2::init()?;
-    let video_subsystem = sdl_context.video()?;
+mod window;
 
-    let window = video_subsystem
-        .window("Nature of Code", 800, 600)
-        .position_centered()
-        .opengl()
-        .build()?;
+struct App {
+    events: sdl2::EventPump,
+    canvas: render::Canvas<video::Window>,
+}
+impl App {
+    pub fn new() -> Result<Self, Box<dyn Error>> {
+        let sdl_context = sdl2::init()?;
+        let video_subsystem = sdl_context.video()?;
 
-    let mut canvas = window.into_canvas().build()?;
+        let window = video_subsystem
+            .window("Nature of Code", 800, 600)
+            .position_centered()
+            .opengl()
+            .build()?;
 
-    canvas.set_draw_color(Color::RGB(255, 255, 255));
-    canvas.clear();
-    canvas.present();
-    let mut event_pump = sdl_context.event_pump()?;
+        let mut canvas = window.into_canvas().build()?;
+        let events = sdl_context.event_pump()?;
 
-    'running: loop {
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
-                _ => {}
-            }
-        }
-
+        canvas.set_draw_color(Color::RGB(255, 255, 255));
         canvas.clear();
         canvas.present();
-        std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
+
+        Ok(Self { canvas, events })
     }
+
+    pub fn run(&mut self) {
+        'running: loop {
+            for event in self.events.poll_iter() {
+                match event {
+                    Event::Quit { .. }
+                    | Event::KeyDown {
+                        keycode: Some(Keycode::Escape),
+                        ..
+                    } => break 'running,
+                    _ => {}
+                }
+            }
+
+            self.canvas.clear();
+            self.canvas.present();
+            std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut app = App::new()?;
+    app.run();
 
     Ok(())
 }
